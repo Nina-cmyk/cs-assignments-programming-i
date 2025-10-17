86245866c74a15dd1e450ef08e5d5bc1896f232e
Nina-cmyk <126146156+Nina-cmyk@users.noreply.github.com>
2025-10-17 09:57:29 +0800

    Add files via upload

---
+++ 414771476_林采霓_第三題/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("請輸入一個整數 n：");
+        int n = ReadInt();
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 5 == 0) Console.WriteLine("特別數字"); 
+            else            Console.WriteLine(i);
+        }
+    }
+
+    static int ReadInt()
+    {
+        while (true)
+        {
+            if (int.TryParse(Console.ReadLine(), out int v)) return v;
+            Console.Write("輸入錯誤，請輸入整數：");
+        }
+    }
+}
