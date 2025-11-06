37dad6ba31577c6935d8c3b201bec55468af9a55
Nina-cmyk <126146156+Nina-cmyk@users.noreply.github.com>
2025-11-06 16:02:21 +0800

    Add files via upload

---
+++ 414771476_林采霓_1106_mid/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 請試用者輸入一個正整數，輸出數字中最大的位數
+        Console.Write("請輸入一個正整數: ");
+        int number = int.Parse(Console.ReadLine());
+        int maxDigit = 0;
+        while (number > 0)
+        {
+            int digit = number % 10;
+            if (digit > maxDigit)
+            {
+                maxDigit = digit;
+            }
+            number /= 10;
+        }
+        Console.WriteLine($"最大的位數是: {maxDigit}");
+            
+        }
+}
